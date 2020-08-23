3535be8bc42fbeebda7f9c14f9c2d0181dd209fc
Sultan <sultan.toktomambetov.04@bk.ru>
2020-08-23 15:29:20 +0600

    filled contacts page.

--- src/Container/contacts/contacts.js
+++ src/Container/contacts/contacts.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import Header from '../../Components/Header/Header';
+import React from "react";
+import { Container, ListGroup } from "react-bootstrap";
+import Header from "../../Components/Header/Header";
+import { NavLink } from "react-router-dom";
 
 export default function contacts(props) {
-    return (
-        <div className="Main">
-            <Header history={props.history} />
-            contacts
-        </div>
-    );
+  return (
+    <div className="Main">
+      <Header history={props.history} />
+      <Container className="pt-5">
+        <ListGroup>
+          <ListGroup.Item>
+            Тел.1 : <NavLink to="#">+#######</NavLink>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Тел.2 : <NavLink to="#">+#######</NavLink>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Instagram : <NavLink to="#">+#######</NavLink>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            telegram : <NavLink to="#">+#######</NavLink>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            facebook : <NavLink to="#">+#######</NavLink>
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
+    </div>
+  );
 }
